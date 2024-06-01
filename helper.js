1fb65f67ac86fb2f4414e5c0cd3fb1a58c41b790
Kiril B <kirbou06012@gmail.com>
2024-06-01 20:27:11 -0300

    optimized counting words and added removing punctuation

--- helper.js
+++ helper.js
@@ -7,31 +7,34 @@ function CountUniqueWords() {
 
   var wordmap = countAllWords(paragraphs)
 
-  let objWordMap = {};
-  for (let [key, value] of wordmap.entries()) {
-    objWordMap[key] = value;
-  }
-  return objWordMap
+  return wordmap
 }
 
 function countAllWords(paragraphs){
-  wordmap = new Map();
+  wordmap = {};
   
   for (var i = 0; i < paragraphs.length; i++) {
     parText = paragraphs[i].getText()
     var words = parText.split(/\s+/);
     for (var i = 0; i < words.length; i++) {
-      var word = words[i].toLowerCase()
-      if (wordmap.has(word)) {
-        var newval = wordmap.get(word) + 1;
-        wordmap.set(word, newval);
+      var word = removePunctuation(words[i].toLowerCase())
+      if (word in wordmap) {
+        wordmap[word]++
       }
       else {
-        wordmap.set(word, 1);
+        wordmap[word] = 1
       }
     }
   }
   return wordmap
+}
+
+function removePunctuation(str) {
+  var punctuationAtEnd = /\W(?=[.,;:!?\)]*$)/g;
+  var cleanedStr = str.replace(punctuationAtEnd, '');
+  var regex = /[\{\}\(\)\[\]]+/g;
+  var cleanedStr = cleanedStr.replace(regex, '');
+  return cleanedStr;
 }
 
 function Display(map){
