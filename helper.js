60de70f6c3c619e238a60fdbbfba39d58093aaa8
Kiril B <kirbou06012@gmail.com>
2024-06-02 16:42:47 -0300

    Fixed incorrect Word Count Bug, and started working on contractions finder.

--- helper.js
+++ helper.js
@@ -1,23 +1,13 @@
 
 function CountUniqueWords() {
-  var doc = DocumentApp.getActiveDocument();
-  var body = doc.getBody();
-  var paragraphs = body.getParagraphs();
+  var paragraphs = getBodyParagraphs()
 
-
-  var wordmap = countAllWords(paragraphs)
-
-  return wordmap
-}
-
-function countAllWords(paragraphs){
   wordmap = {};
-  
   for (var i = 0; i < paragraphs.length; i++) {
     parText = paragraphs[i].getText()
     var words = parText.split(/\s+/);
-    for (var i = 0; i < words.length; i++) {
-      var word = removePunctuation(words[i].toLowerCase())
+    for (var j = 0; j < words.length; j++) {
+      var word = removePunctuation(words[j].toLowerCase())
       if (word in wordmap) {
         wordmap[word]++
       }
@@ -26,16 +16,54 @@ function countAllWords(paragraphs){
       }
     }
   }
+
   return wordmap
 }
 
+function FindContractions(){
+  var paragraphs = getBodyParagraphs()
+
+  var contractions = getContractions()
+  var contractionsFound = {}
+  for (var i = 0; i < paragraphs.length; i++) {
+
+    parText = paragraphs[i].getText()
+    var words = parText.split(/\s+/);
+
+    for (var j = 0; j < words.length; j++) {
+      var word = String(removePunctuation(words[j].toLowerCase()))
+      
+      if (word in contractions && word in contractionsFound) {
+        contractionsFound[word]['count'] += 1
+      } else if (word in contractions) {
+        contractionsFound[word] = {
+          'count': 1,
+          'from': contractions[word]
+        }
+      }
+    }
+  }
+  return contractionsFound
+}
+
+function getBodyParagraphs(){
+  return DocumentApp.getActiveDocument().getBody().getParagraphs()
+}
+
+function getContractions(){
+  var userProperties = PropertiesService.getUserProperties();
+  var contractionsString = userProperties.getProperty('list');
+  return JSON.parse(contractionsString);
+}
+
 function removePunctuation(str) {
-  var punctuationAtEnd = /\W(?=[.,;:!?\)]*$)/g;
+  var punctuationAtEnd = /\W(?=[.,;:!?\")]*$)/g;
   var cleanedStr = str.replace(punctuationAtEnd, '');
   var regex = /[\{\}\(\)\[\]]+/g;
   var cleanedStr = cleanedStr.replace(regex, '');
-  return cleanedStr;
+  return cleanedStr.replace(/[\u2018\u2019]/g, "'");
 }
+
 
 function Display(map){
   for (let [key, value] of map) {
--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 function onOpen(){
   DocumentApp.getUi().createAddonMenu()
     .addItem('Count Unique Words', 'CreateCountUniqueWords')
+    .addItem('Find Contractions', 'CreateContractions')
     .addToUi()
 }
 
 function CreateCountUniqueWords() {
-  var html = HtmlService.createTemplateFromFile("Index").evaluate().setTitle("Word Counter")
+  var html = HtmlService.createTemplateFromFile("CountWords").evaluate().setTitle("Word Counter")
+  DocumentApp.getUi().showSidebar(html)
+}
+
+function CreateContractions(){
+  var html = HtmlService.createTemplateFromFile("Contractions").evaluate().setTitle("Contractions")
   DocumentApp.getUi().showSidebar(html)
 }
 
